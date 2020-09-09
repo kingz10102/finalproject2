aeb889421fb4bacfde74adaea6750a5bddd3bd63
kingz10102 <kingz10102@gmail.com>
2020-09-09 01:01:26 -0400

    this is a commit

--- src/components_folder/Login.jsx
+++ src/components_folder/Login.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
-import { Form, FormGroup, Label, Button, Input }
+import { Form, FormGroup, Label, Button, Input } from 'reactstrap';
+
 class Login extends Component {
-   render() {
-    return (
-        <Form className="Login-page">
-            <h1>Sign In</h1>
-        </Form>
-    );
-}
+    render() {
+        return (
+            <Form className="login-page">
+                <h1>
+                    <span className="font-weight-bold">Login Kick Users</span>
+                </h1>
+            </Form>
+        )
+    }
 }
 
-export default Login
+export default Login;
