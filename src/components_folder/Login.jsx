a31387b667920dd07475cd5fb9dac310d75961f9
kingz10102 <kingz10102@gmail.com>
2020-09-09 00:49:07 -0400

    this is a commit

--- src/components_folder/Login.jsx
+++ src/components_folder/Login.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-
+import { Form, FormGroup,}
 class Login extends Component {
    render() {
     return (
         <Form className="Login-page">
             <h1>Sign In</h1>
         </Form>
-    )
+    );
 }
 }
 
