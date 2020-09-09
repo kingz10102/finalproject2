79f028e0f05aae5f02c4710531d7ffedf165166e
kingz10102 <kingz10102@gmail.com>
2020-09-09 00:27:34 -0400

    this is a commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Heading from './components_folder/Heading';
 import Sneakers from './components_folder/Sneakers';
 import Home_Page from './components_folder/Home_Page';
-import'./App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Socks from './components_folder/Socks';
 import Login from './components_folder/Login';
+import'./App.css';
 
 
 function App()  {
