f8da437ef901a7b53b000a757ae514d53291856e
kingz10102 <kingz10102@gmail.com>
2020-09-09 00:47:38 -0400

    this is a commit

--- src/components_folder/Login.jsx
+++ src/components_folder/Login.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function Login() {
+class Login extends Component {
+   render() {
     return (
-        <div>
-            <h1>hello</h1>
-        </div>
+        <Form className="Login-page">
+            <h1>Sign In</h1>
+        </Form>
     )
+}
 }
 
 export default Login
